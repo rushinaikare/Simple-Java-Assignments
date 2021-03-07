9b5bc2fad071183ba696f6a38b230e615b44d1fc
Rushikesh Naikare <53118197+rushinaikare@users.noreply.github.com>
2021-03-07 19:05:51 +0530

    Add files via upload

---
+++ AnagramCheck.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class AnagramCheck {
+
+	static boolean isAnagram(String a, String b) {
+        // Complete the function
+        String string1 = a.replaceAll("\\s", "");
+        String string2 = b.replaceAll("\\s", "");
+        boolean status = false;
+        if(string1.length() == string2.length()){
+            char [] str1 = string1.toLowerCase().toCharArray();
+            char [] str2 = string2.toLowerCase().toCharArray();
+            Arrays.sort(str1); 
+            Arrays.sort(str2);
+            status = Arrays.equals(str1, str2);
+             return status;
+        }else
+           status = false;
+           return status;
+        
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean result = AnagramCheck.isAnagram("ABcd", "abCD");
+		System.out.println( result ? "Anagram" : "Not Anagram" );
+		
+		
+	}
+
+}
