38620e441bb6d463655db0b01884d88892a0c244
Rushikesh Naikare <53118197+rushinaikare@users.noreply.github.com>
2021-03-07 19:12:33 +0530

    Update AnagramCheck.java

--- AnagramCheck.java
+++ AnagramCheck.java
@@ -1,3 +1,5 @@
+//Write a Java program to check whether two strings are anagram or not?
+
 import java.util.Arrays;
 
 public class AnagramCheck {
@@ -8,17 +10,17 @@ public class AnagramCheck {
         String string2 = b.replaceAll("\\s", "");
         boolean status = false;
         if(string1.length() == string2.length()){
-            char [] str1 = string1.toLowerCase().toCharArray();
-            char [] str2 = string2.toLowerCase().toCharArray();
-            Arrays.sort(str1); 
-            Arrays.sort(str2);
-            status = Arrays.equals(str1, str2);
-             return status;
-        }else
-           status = false;
-           return status;
+               char [] str1 = string1.toLowerCase().toCharArray();
+               char [] str2 = string2.toLowerCase().toCharArray();
+               Arrays.sort(str1); 
+               Arrays.sort(str2);
+               status = Arrays.equals(str1, str2);
+               return status;
+              }else
+               status = false;
+               return status;
         
-    }
+            }
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
